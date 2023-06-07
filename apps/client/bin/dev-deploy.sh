31b75786df39c2cbe2fe4de24dbb12193220450e
ash <ashley@digicodeltd.com>
2023-06-07 19:50:59 +0100

    fix: client dev-deploy script

--- apps/client/bin/dev-deploy.sh
+++ apps/client/bin/dev-deploy.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
 
-aws s3 sync --delete ./dist s3://dev-ash-actions-monorepo
-aws cloudfront create-invalidation --distribution-id E1EMJ7IEHO56RP --paths /index.html
+DIST=./dist
+
+if [[ "$GITHUB_ACTIONS" == "true" ]]; then
+  DIST=apps/client/dist
+fi
+
+if [ ! -d "$DIST" ]; then
+  echo "'$DIST' does not exist. Exiting script."
+  exit 1
+fi
+
+aws s3 sync --delete $DIST s3://dev-ash-actions-monorepo
+aws cloudfront create-invalidation --distribution-id E1EMJ7IEHO56RP --paths /*
