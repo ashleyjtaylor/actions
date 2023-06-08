f053ef582dc5855db42cb85aeb5d0c6aaa9c7023
ash <ashley@digicodeltd.com>
2023-06-08 17:47:04 +0100

    fix: oidc role perms?

--- infrastructure/lib/constructs/OIDCProvider.ts
+++ infrastructure/lib/constructs/OIDCProvider.ts
@@ -42,8 +42,8 @@ export default class OIDCProvider extends Construct {
           statements: [
             new PolicyStatement({
               sid: 'CdkDeploymentPermissions',
-              actions: ['sts:AssumeRole'],
-              resources: ['arn:aws:iam:::role/cdk-*'],
+              actions: ['sts:AssumeRole', 'iam:PassRole'],
+              resources: ['arn:aws:iam:::role/cdk-hnb659fds-*'],
               effect: Effect.ALLOW
             }),
             new PolicyStatement({
