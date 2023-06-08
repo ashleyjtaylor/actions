57ab19ae0b66855e46b5476cb123987cc5738f4e
ash <ashley@digicodeltd.com>
2023-06-08 20:48:48 +0100

    fix(client): deploy script

--- apps/client/bin/dev-deploy.sh
+++ apps/client/bin/dev-deploy.sh
@@ -7,7 +7,8 @@ if [ ! -d "$DIST" ]; then
   exit 1
 fi
 
-BUCKET=$(aws ssm get-parameter --name /dev/actions/client/bucketName --query 'Parameter.Name')
-CLOUD_FRONT_DISTRIBUTION_ID=$(aws ssm get-parameter --name /dev/actions/client/cloudFrontDistributionId || echo "Could not retrieve CloudFront Distribution Id" && exit 1)
+BUCKET=$(aws ssm get-parameter --name /dev/actions/client/bucketName --query 'Parameter.Value')
+echo $BUCKET
+CLOUD_FRONT_DISTRIBUTION_ID=$(aws ssm get-parameter --name /dev/actions/client/cloudFrontDistributionId --query 'Parameter.Value' || echo "Could not retrieve CloudFront Distribution Id" && exit 1)
 aws s3 sync --delete $DIST s3://$BUCKET
 aws cloudfront create-invalidation --distribution-id $CLOUD_FRONT_DISTRIBUTION_ID --paths /*
