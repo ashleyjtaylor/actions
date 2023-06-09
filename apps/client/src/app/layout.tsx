573bcd7a177af903d05f6a68d4139ac7a9a4c300
ash <ashley@digicodeltd.com>
2023-06-09 19:34:37 +0100

    chore: tidy workflows. client update

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
+        <h1>Welcome to Next.js on AWS S3</h1>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/server">Server</Link>
