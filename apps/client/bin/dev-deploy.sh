9be5662835cdc0ad8083c6b69bd875c6971453ff
ash <ashley@digicodeltd.com>
2023-10-20 19:28:35 +0100

    chore: command fixes and rename file

--- apps/client/bin/dev-deploy.sh
+++ apps/client/bin/dev-deploy.sh
@@ -10,4 +10,4 @@ fi
 BUCKET=$(aws ssm get-parameter --name /dev/actions/client/bucketName --output text --query 'Parameter.Value' || echo "Could not retrieve Bucket Name" && exit 1)
 CLOUD_FRONT_DISTRIBUTION_ID=$(aws ssm get-parameter --name /dev/actions/client/cloudFrontDistributionId --output text --query 'Parameter.Value' || echo "Could not retrieve CloudFront Distribution Id" && exit 1)
 aws s3 sync --delete $DIST s3://$BUCKET
-aws cloudfront create-invalidation --distribution-id $CLOUD_FRONT_DISTRIBUTION_ID --paths /*
+aws cloudfront create-invalidation --distribution-id $CLOUD_FRONT_DISTRIBUTION_ID --paths /index.html
